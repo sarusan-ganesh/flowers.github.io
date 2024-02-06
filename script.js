fdbe49f52e82177a3624f1227ac114e280e2fc8a
sarusan-ganesh <159059282+sarusan-ganesh@users.noreply.github.com>
2024-02-06 02:04:32 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-
 onload = () => {
-  const c = setTimeout(() => {
-    document.body.classList.remove("not-loaded");
-    clearTimeout(c);
-  }, 1000);
+    const c = setTimeout(() => {
+        document.body.classList.remove("not-loaded");
+        clearTimeout(c);
+    }, 1000);
 };
+
+// Add the crosswordButton event listener
+document.getElementById('crosswordButton').addEventListener('click', function() {
+    window.location.href = 'https://crosswordlabs.com/view/not-so-mini-crossword';
+});
